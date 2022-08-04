6bb225323ea37f36ab6527fc6e49df923b196bf7
basidialsnail <dmmerchant@gmail.com>
2022-08-04 19:13:00 -0400

    faile to save server.js prior to git push

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const api = require('./routes/index.js');
 const { mware } = require('./middleware/mware');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(mware)
 
